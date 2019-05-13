b10d169cc9e52c32943335729c7bd440a139d7c5
davidschlundtbodien <d.schlundtbodien@gmail.com>
2019-05-12 22:33:54 -0700

    adds actor url query options

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,6 +1,22 @@
 class Api::ActorsController < ApplicationController
-  def single_actor
-    @actor = Actor.find_by(id: 3)
+  #def single_actor
+  #  @actor = Actor.find_by(id: 3)
+  #  render 'single_actor.json.jbuilder'
+  #end
+  def actor_query
+    @actor = Actor.find(params[:id])
     render 'single_actor.json.jbuilder'
   end
+
+  def actor_segment
+    @actor = Actor.find(params[:id])
+    render 'single_actor.json.jbuilder'
+  end
+  
+  def actor_body
+    @actor = Actor.find(params[:id])
+    render 'single_actor.json.jbuilder'   
+  end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/single_actor" => "actors#single_actor"
+    get "/actor" => "actors#actor_query"
+    get "/actor/:id" => "actors#actor_segment"
+    post "/actor" => "actors#actor_body"
     #Movie routes w/ REST syntax
     get "/movies/:id" => "movies#show"
     get "/movies" => "movies#index"
