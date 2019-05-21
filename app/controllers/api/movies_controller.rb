39e69b3fe2ed875a1f34e03f6cebd00bd5834863
davidschlundtbodien <d.schlundtbodien@gmail.com>
2019-05-20 21:23:16 -0700

    adds ActiveRecord queries to movie and actor index

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -5,7 +5,7 @@ class Api::MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all.where(english: true)
     render 'index.json.jbuilder' 
   end
 
