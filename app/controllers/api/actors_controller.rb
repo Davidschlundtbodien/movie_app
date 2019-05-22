32867be9758ffce8ccd7c8881720a11f1462ffdd
davidschlundtbodien <d.schlundtbodien@gmail.com>
2019-05-21 20:55:32 -0700

    adds assocation between movie and actors

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -16,7 +16,8 @@ class Api::ActorsController < ApplicationController
       last_name: params[:last_name],
       gender: params[:gender],
       age: params[:age],
-      known_for:params[:known_for]
+      known_for:params[:known_for],
+      movie_id:params[:movie_id]
       )
     if @actor.save
       render 'show.json.jbuilder'
@@ -32,6 +33,7 @@ class Api::ActorsController < ApplicationController
     @actor.gender = params[:gender]  || @actor.gender
     @actor.age = params[:age]  || @actor.age
     @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.movie_id = params[:movie_id] || @actor.movie_id
 
     if @actor.save
       render 'show.json.jbuilder'
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  
+  has_many :actors
+
   validates :title, :plot, presence: true
   validates :year, numericality: { only_integer: true }
   validates :plot, length: { in: 20..500}
